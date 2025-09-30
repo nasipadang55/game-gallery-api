8495529bff4474a240dbc1ab2025bf49a15df4e0
nasipadang55 <nasipadangcipal@gmail.com>
2025-09-30 19:00:22 +0700

    Update 5g.js

--- api/5g.js
+++ api/5g.js
@@ -1,22 +1,5 @@
-// /api/5g.js
-import fs from "fs";
-import path from "path";
+import games from "../data/5g.json" assert { type: "json" };
 
 export default function handler(req, res) {
-  const dir = path.join(process.cwd(), "public/images/5g");
-
-  // ambil semua nama file di folder
-  const files = fs.readdirSync(dir);
-
-  // mapping ke format JSON
-  const games = files.map((filename) => {
-    const id = path.parse(filename).name; // nama file tanpa ekstensi
-    return {
-      id,
-      title: id.replace(/-/g, " "), // opsional: bikin judul dari nama file
-      imageUrl: `/images/5g/${filename}`,
-    };
-  });
-
   res.status(200).json(games);
 }
