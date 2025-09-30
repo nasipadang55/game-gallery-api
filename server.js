de389ef5f09acd003931d2d9abe47b45d9e1d1eb
nasipadang55 <nasipadangcipal@gmail.com>
2025-09-30 14:56:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+export default function handler(req, res) {
+  const games = [
+    { id: 'KYS-H5-99980', title: 'KYS-H5-99980', imageUrl: '/images/5g/KYS-H5-99980.png', provider: '5g', providerName: '5G' },
+    { id: 'KYS-H5-99981', title: 'KYS-H5-99981', imageUrl: '/images/5g/KYS-H5-99981.png', provider: '5g', providerName: '5G' },
+    { id: 'KYS-H5-99982', title: 'KYS-H5-99982', imageUrl: '/images/5g/KYS-H5-99982.png', provider: '5g', providerName: '5G' },
+  ];
+
+  res.status(200).json(games);
+}
+
+
 // Serve images folder
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
