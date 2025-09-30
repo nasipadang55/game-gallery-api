e8de3e710f42d72053f8c4fd0c7484299abccbf6
nasipadang55 <nasipadangcipal@gmail.com>
2025-09-30 19:59:22 +0700

    Update 5g.js

--- api/5g.js
+++ api/5g.js
@@ -3,16 +3,13 @@ const path = require('path');
 
 module.exports = (req, res) => {
   try {
-    const imagesFolder = path.join(__dirname, '../public/images/5g');
-    if (!fs.existsSync(imagesFolder)) {
-      return res.status(404).json({ error: 'Folder tidak ditemukan' });
-    }
-
+    const imagesFolder = path.join(__dirname, 'images/5g');
     const files = fs.readdirSync(imagesFolder);
+
     const data = files.map(file => ({
       id: path.parse(file).name,
       title: path.parse(file).name,
-      imageUrl: `/images/5g/${file}`
+      imageUrl: `/api/images/5g/${file}`
     }));
 
     res.setHeader('Access-Control-Allow-Origin', '*');
