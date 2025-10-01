72cd721f067d559a796ff841affae5f6fb37245c
nasipadang55 <nasipadangcipal@gmail.com>
2025-10-01 13:38:40 +0700

    Update 5g.js

--- api/5g.js
+++ api/5g.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = (req, res) => {
   try {
     // Path ke folder public/images/5g
-    const imagesFolder = path.join(process.cwd(), '/images/5g');
+    const imagesFolder = path.join(process.cwd(), 'public/images/5g');
     const files = fs.readdirSync(imagesFolder);
 
     // Filter file gambar saja
