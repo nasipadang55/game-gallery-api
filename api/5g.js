dec264036ecaa8ae1c289bcf33f608e01724ae77
nasipadang55 <nasipadangcipal@gmail.com>
2025-10-01 14:20:12 +0700

    Update 5g.js

--- api/5g.js
+++ api/5g.js
@@ -14,7 +14,7 @@ module.exports = (req, res) => {
     const data = imageFiles.map(file => ({
       id: path.parse(file).name,
       title: path.parse(file).name,
-      imageUrl: `https://sansserif.cloud/images/5g/${file}`
+      imageUrl: `public/images/5g/${file}`
     }));
 
     res.setHeader('Access-Control-Allow-Origin', '*');
